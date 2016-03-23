99656837a0d6bf3cd82893e95b79a939247c1749
Salem <Salem@10.200.2.107>
2016-03-23 18:04:47 -0500

    Improved and added a bit more detail to my java doc.

---
+++ test/nbody/BodyTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nbody;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Salem
+ */
+public class BodyTest {
+    
+    public BodyTest() {
+    }
+
+    /**
+     * Test of move method, of class Body.
+     */
+    @Test
+    public void testMove() {
+        System.out.println("move");
+        Vector f = null;
+        double dt = 0.0;
+        Body instance = null;
+        instance.move(f, dt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of update method, of class Body.
+     */
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        Vector r = null;
+        Body instance = null;
+        instance.update(r);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of forceFrom method, of class Body.
+     */
+    @Test
+    public void testForceFrom() {
+        System.out.println("forceFrom");
+        Body b = null;
+        Body instance = null;
+        Vector expResult = null;
+        Vector result = instance.forceFrom(b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of repel method, of class Body.
+     */
+    @Test
+    public void testRepel() {
+        System.out.println("repel");
+        double radius = 0.0;
+        Body instance = null;
+        instance.repel(radius);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of drawtrail method, of class Body.
+     */
+    @Test
+    public void testDrawtrail() {
+        System.out.println("drawtrail");
+        Body instance = null;
+        instance.drawtrail();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of draw method, of class Body.
+     */
+    @Test
+    public void testDraw_0args() {
+        System.out.println("draw");
+        Body instance = null;
+        instance.draw();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of draw method, of class Body.
+     */
+    @Test
+    public void testDraw_double() {
+        System.out.println("draw");
+        double penRadius = 0.0;
+        Body instance = null;
+        instance.draw(penRadius);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
